d5f8fb41463b6df61bee3331ae0f36bcc7ba1882
Gypsophila。 <futur_wxc@163.com>
2021-09-26 19:41:40 +0800

    增加跳转至 Profile 页，未传 username 处理

--- middleware/profile.js
+++ middleware/profile.js
@@ -2,10 +2,14 @@
 export default function({
   redirect,
   params: { username },
-  query: { favorited, author }
+  query: { favorited, author },
+  store
 }) {
   if (favorited || author) {
     return
+  }
+  if (!username) {
+    username = store.state.user.username
   }
   return redirect(`/profile/${username}?author=${username}`)
 }
